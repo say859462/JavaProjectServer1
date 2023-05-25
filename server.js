d7ab67834e87adf5e70e315e01b65091a7373e9a
say859462 <say859462@yahoo.com.tw>
2023-05-25 23:50:44 +0800

    new

--- server.js
+++ server.js
@@ -2,14 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.get("/timer/:duration/:username", (req, res) => {
-  const { duration, username } = req.params;
+app.get("/:duration/:username/:phone?", (req, res) => {
+  const { duration, username, phone } = req.params;
 
   setTimeout(() => {
     // 在這裡執行你想要執行的動作
 
     // 例如印出使用者名稱
-    console.log("執行動作 - 使用者：", username);
+       if (username) {
+         // 使用者名稱參數存在
+         console.log("使用者名稱:", username);
+       }
+
+       if (phone) {
+         // 電話號碼參數存在
+         console.log("電話號碼:", phone);
+       }
+
   }, duration * 1000); // 將秒數轉換為毫秒
 
   res.send("Timer started");
