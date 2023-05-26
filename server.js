82608b38121fa16d028ee537e6bfa07008631af1
say859462 <say859462@yahoo.com.tw>
2023-05-26 14:57:41 +0800

    del

--- server.js
+++ server.js
@@ -1,29 +1,16 @@
 const express = require("express");
 const app = express();
 
-const MailController = require("./MailController");
-const mailController = new MailController();
-
-const SMSController = require("./SMSController");
-const smsController = new SMSController();
-
-app.get("/:duration", (req, res) => {
+app.get("/:duration", async (req, res) => {
   const { duration } = req.params;
   const { username, phone } = req.query;
+
   // setTimeout(() => {
   //   // 在這裡執行你想要執行的動作
   //   // 例如印出使用者名稱
 
   // }, duration * 1000); // 將秒數轉換為毫秒
-  if (username) {
-    mailController.sendMail(username);
-    console.log("寄送電子郵件");
-  }
-
-  if (phone) {
-    smsController.sendSMS(phone);
-    console.log("寄送手機簡訊");
-  }
+  
   res.send("Timer started");
 });
 
